0aa9717f81e35babc5b8509457f9eac3ae5d9699
Boryslavsky2003 <temp2003late@gmail.com>
2025-01-18 20:31:03 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const StartApp());
+}
+
+class StartApp extends StatelessWidget {
+  const StartApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
