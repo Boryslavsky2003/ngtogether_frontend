63c6b19f7bdb68d709b12b76da9b23c10363b432
Boryslavsky2003 <temp2003late@gmail.com>
2025-01-19 16:38:53 +0200

    color

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryColor = Color(0xFF);
-  static const Color secondaryColor = Color(0xFF);
+  static const Color primaryColor = Color(0x000000ff);
+  static const Color secondaryColor = Color(0x000000ff);
   static const Color backgroundColor = Color(0xFF055766);
-  static const Color textColor = Color(0xFF);
-  static const Color accentColor = Color(0xFF);
+  static const Color textColor = Color(0x000000ff);
+  static const Color accentColor = Color(0x000000ff);
 }
